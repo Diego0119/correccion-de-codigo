982fa07e1bf5047b60b68b60be96ffbbada8ae83
Diego0119 <diegosanhuezaaguilar@gmail.com>
2024-11-07 19:39:29 -0300

    solucionando la caida al presionar el logo

--- header.php
+++ header.php
@@ -41,7 +41,7 @@ $tipoUsuario = $_SESSION['tipo'];
     <!--INICIO  Header  Exp2 -->
     <header id="header" class="header fixed-top d-flex align-items-center">
         <div class="d-flex align-items-center justify-content-between">
-            <a href="index.html" class="logo d-flex align-items-center">
+            <a href="" class="logo d-flex align-items-center">
                 <img src="/Notas/img/logo.png" alt="">
                 <span class="d-none d-lg-block">EduScore</span>
             </a>
